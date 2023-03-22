1b8b6e15c5296ffdcf340add0fcdc70e0599ecb6
ladama2016 <125318776+ladama2016@users.noreply.github.com>
2023-03-21 22:48:21 -0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,65 +1,37 @@
-function recibir()
-{
-  var cedula=document.getElementById("id").value;
-  let numero0=0;
-  let numero1=0;
-  let numero2=0;
-  let numero3=0;
-  let verificador= 0;     
-  var suma=0;
-  
-  for (let i = 10; i>=0; i--){
-    
-    if (i%2!==0)
+fetch(`json.json`)
+.then(data => data.json())
+.then (data=> 
     {
-      numero0=cedula[i]*=2;
-    }
-    
-    else 
-    {
-      numero0=cedula[i];
-    } 
-    
-    if (numero0>=10)
-    {
-      numero0=numero0.toString();
-      numero1=parseInt(numero0[0]);
-      numero2=parseInt(numero0[1]);
-      
-      numero0=numero1+=numero2;
-   } 
-    
-    
-    suma=suma+parseInt(numero0);
+        let keys = Object.keys(data.sub_menu);
+        for(let i=18; i>= 0; i--)
+        {
+          let key=keys[i];
 
-  }
-  suma-=cedula[10];
-  suma=suma*9;
+                let menu = document.createElement('li')
+                let ref= document.createElement('a')
+                let texto = document.createTextNode(''+data.sub_menu[key])
+                menu.appendChild(ref);
+                menu.appendChild(texto);
 
-  suma=suma.toString();
-  numero1=parseInt(suma[0]);
-  numero2=parseInt(suma[1]);
-  numero3=parseInt(suma[2]);
-  
-  verificador=numero3;
-  
-  document.getElementById("respuesta").innerHTML = " se han terminado de ingresar los datos el resultado es ";
-  
-  if (verificador==cedula[10])
-  {
-    document.getElementById("respuesta").innerHTML = "el numero de cedula ingresado es correcto "; 
-  }
-  
-  else
-  {
-    document.getElementById("respuesta").innerHTML = " el numero ingresado no es corrrecto.el verificaador deberia ser "+verificador;
-  }
- 
-    
-}
+                if(i>=12)
+                {
+                    document.getElementsByClassName('sub-mini1')[0].appendChild(menu);
+                }
 
-function borra() 
-{
-  
+                if(i<=11)
+                {
+                    document.getElementsByClassName('sub-mini2')[0].appendChild(menu);
+                }
 
-}
+                if(i<7)
+                {
+                    document.getElementsByClassName('sub-mini3')[0].appendChild(menu);
+                }
+
+           
+
+        }
+        
+    })
+
+
