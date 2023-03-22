876b027540be9e5a362ee8e6e02e629b17f19b32
ladama2016 <125318776+ladama2016@users.noreply.github.com>
2023-03-22 00:54:32 -0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -3,6 +3,35 @@ fetch(`json.json`)
 .then (data=> 
     {
         let keys = Object.keys(data.sub_menu);
+        let ks=Object.keys(data.menu);
+
+        for(let i=3; i>=0; i--)
+        {
+            let k=ks[i];
+
+            let menu = document.createElement('li')
+                let ref= document.createElement('a')
+                let texto = document.createTextNode(''+data.menu[k])
+                menu.appendChild(ref);
+                menu.appendChild(texto);
+
+            if(i==3 ||i==2||i==0)
+            {
+                let sub = document.createElement('ul')
+                sub.classList.add("sub-mini"+i);
+                menu.appendChild(sub);
+                
+            }
+
+            else 
+            {
+
+            }
+
+            document.getElementsByClassName('menu-mini')[0].appendChild(menu);
+
+        }
+
         for(let i=18; i>= 0; i--)
         {
           let key=keys[i];
@@ -15,7 +44,7 @@ fetch(`json.json`)
 
                 if(i>=12)
                 {
-                    document.getElementsByClassName('sub-mini1')[0].appendChild(menu);
+                    document.getElementsByClassName('sub-mini3')[0].appendChild(menu);
                 }
 
                 if(i<=11)
@@ -25,10 +54,8 @@ fetch(`json.json`)
 
                 if(i<7)
                 {
-                    document.getElementsByClassName('sub-mini3')[0].appendChild(menu);
+                    document.getElementsByClassName('sub-mini0')[0].appendChild(menu);
                 }
-
-           
 
         }
         
