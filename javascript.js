dfab68d931554bf6d282b9e99f333a44ad8302b5
ladama2016 <125318776+ladama2016@users.noreply.github.com>
2023-04-13 22:35:57 -0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,57 +1,37 @@
-fetch(`json.json`)
-.then(data => data.json())
-.then (data=> 
+const enviar=document.querySelector(".enviar");
+
+enviar.addEventListener("click",function(e)
+{
+    e.preventDefault();
+
+    send();
+
+});
+
+
+function send()
+{
+   
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("last").value;
+    var compañia = document.getElementById("compañia").value;
+    var telefono = document.getElementById("telefono").value;
+    var etiqueta_t = document.getElementById("etiqueta_t").value;
+    var email = document.getElementById("email").value;
+    var etiqueta_h = document.getElementById("etiqueta_h").value;
+    var fecha = document.getElementById("fecha").value;
+
+    var url="BEGIN:VCARD\nVERSION:3.0\nN:"+apellido+";"+nombre+";;;"+"\nORG:"+compañia+"\nTEL;TYPE="+etiqueta_t+":+"+telefono+"\nEMAIL;"+"TYPE="+etiqueta_h+":"+email+"\nBDAY:"+fecha+"\nEND:VCARD";
+
+    new QRCode(document.getElementById("qr"),
     {
-        let keys = Object.keys(data.sub_menu);
-        let ks=Object.keys(data.menu);
-
-        for(let i=3; i>=0; i--)
-        {
-            let k=ks[i];
-
-                
-                let menu = document.createElement("li")
-                let a= document.createElement("a")
-                a.setAttribute("href","https://www.banreservas.com/");
-                let texto = document.createTextNode(''+data.menu[k])
-                let sub = document.createElement("ul")
-                sub.classList.add("sub-mini"+i);
-                
-                menu.appendChild(a);
-                menu.appendChild(texto);
-                menu.appendChild(sub);
-            
-            document.getElementsByClassName('menu-mini')[0].appendChild(menu);
-
-        }
-
-        for(let i=18; i>= 0; i--)
-        {
-          let key=keys[i];
-
-                let menu = document.createElement('li')
-                let ref= document.createElement('a')
-                let texto = document.createTextNode(''+data.sub_menu[key])
-                menu.appendChild(ref);
-                menu.appendChild(texto);
-
-                if(i>=12)
-                {
-                    document.getElementsByClassName('sub-mini3')[0].appendChild(menu);
-                }
-
-                if(i<=11)
-                {
-                    document.getElementsByClassName('sub-mini2')[0].appendChild(menu);
-                }
-
-                if(i<7)
-                {
-                    document.getElementsByClassName('sub-mini0')[0].appendChild(menu);
-                }
-
-        }
-        
+        text: url,
+        wiidth:256,
+        height:256,
+        height: 256,
+        colorDark : "#000000",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.H
     })
 
-
+}
